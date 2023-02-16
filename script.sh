4ae3ea72d47090ded5291cf981e0918bd1d79c14
Morten Søe Pedersen <22166060+mortenp1337@users.noreply.github.com>
2023-02-16 08:18:42 +0000

    add json file example

--- script.sh
+++ script.sh
@@ -10,3 +10,14 @@ curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$orgname/$reponame/dispatches \
   -d '{"event_type":"on-demand-test","client_payload":{"input1":"first input","input2":"second input"}}'
+
+
+# alternative use a file
+  curl \
+  -v \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $pat"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$orgname/$reponame/dispatches \
+  -d @jsonpayload.json
